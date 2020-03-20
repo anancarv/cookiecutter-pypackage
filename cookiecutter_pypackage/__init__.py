6d3ae38b8d31d4271d6ab6c65b29f195a8b31344
Ananias CARVALHO <carvalhoananias@hotmail.com>
2020-03-20 18:25:34 +0100

    feat: add cookiecutter integration

--- cookiecutter_pypackage/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Top-level package for {{ cookiecutter.project_name }}."""
-
-__version__ = "0.1.0"
---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -0,0 +1,3 @@
+"""Top-level package for {{ cookiecutter.project_slug }}."""
+
+__version__ = "0.1.0"
