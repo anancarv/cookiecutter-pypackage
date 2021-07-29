a68c538fa06f791acdd033d05ea99b5d1d8e0fa7
Ananias CARVALHO <carvalhoananias@hotmail.com>
2021-07-29 21:19:40 +0200

    feat: add logging

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,3 +1,11 @@
-"""Top-level package for {{ cookiecutter.project_slug }}."""
+"""
+Top-level package for {{ cookiecutter.project_slug }}.
+"""
+import logging
 
 __version__ = "{{cookiecutter.project_version}}"
+
+logging.basicConfig(format="%(asctime)s %(levelname)s:%(name)s: %(message)s")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.info(f"Version {__version__}")
